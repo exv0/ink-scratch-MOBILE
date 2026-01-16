35274bd70b8a971681d44175d6ce3589707ba28a
blissexodia <binnoley@gmail.com>
2026-01-16 12:49:21 +0545

    auth usecase

---
+++ lib/features/auth/data/providers/auth_usecase_providers.dart
@@ -0,0 +1,24 @@
+// lib/features/auth/data/providers/auth_usecase_providers.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ink_scratch/features/auth/domain/usecases/login_usecase.dart';
+import 'package:ink_scratch/features/auth/domain/usecases/register_usecase.dart';
+import 'package:ink_scratch/features/auth/domain/usecases/logout_usecase.dart';
+import 'package:ink_scratch/features/auth/data/providers/auth_repository_provider.dart';
+
+/// Provides the LoginUseCase
+final loginUseCaseProvider = Provider<LoginUseCase>((ref) {
+  final repo = ref.read(authRepositoryProvider);
+  return LoginUseCase(authRepository: repo);
+});
+
+/// Provides the RegisterUseCase
+final registerUseCaseProvider = Provider<RegisterUseCase>((ref) {
+  final repo = ref.read(authRepositoryProvider);
+  return RegisterUseCase(authRepository: repo);
+});
+
+/// Provides the LogoutUseCase
+final logoutUseCaseProvider = Provider<LogoutUseCase>((ref) {
+  final repo = ref.read(authRepositoryProvider);
+  return LogoutUseCase(authRepository: repo);
+});
