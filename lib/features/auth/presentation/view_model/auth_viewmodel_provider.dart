6e5f418a78295e76421778f7b8cd907bef907068
blissexodia <binnoley@gmail.com>
2026-01-16 12:51:11 +0545

    auth viewmodel

---
+++ lib/features/auth/presentation/view_model/auth_viewmodel_provider.dart
@@ -0,0 +1,19 @@
+// lib/features/auth/presentation/view_model/auth_viewmodel_provider.dart
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ink_scratch/features/auth/presentation/state/auth_state.dart';
+import 'package:ink_scratch/features/auth/presentation/view_model/auth_viewmodel.dart';
+import 'package:ink_scratch/features/auth/data/providers/auth_usecase_providers.dart';
+
+final authViewModelProvider = StateNotifierProvider<AuthViewModel, AuthState>((
+  ref,
+) {
+  final loginUseCase = ref.read(loginUseCaseProvider);
+  final registerUseCase = ref.read(registerUseCaseProvider);
+  final logoutUseCase = ref.read(logoutUseCaseProvider);
+
+  return AuthViewModel(
+    loginUseCase: loginUseCase,
+    registerUseCase: registerUseCase,
+    logoutUseCase: logoutUseCase,
+  );
+});
