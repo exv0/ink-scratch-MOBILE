227abcc471f280e09454ad2c4ef726ec45aa61d8
blissexodia <binnoley@gmail.com>
2026-01-30 12:53:57 +0545

    physical switch

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -6,11 +6,12 @@ class AppConfig {
   // static const String baseUrl = 'http://10.0.2.2:3000/api/auth';
 
   // For iOS Simulator or Web (running Flutter on same machine as backend):
-  static const String baseUrl = 'http://localhost:3000/api/auth';
+  // static const String baseUrl = 'http://localhost:3000/api/auth';
 
   // For Physical Device (replace with your computer's IP):
   // Find your IP: Run 'ipconfig' on Windows, look for IPv4 Address
-  // static const String baseUrl = 'http://192.168.1.70:3000/api/auth';
+
+  static const String baseUrl = 'http://192.168.1.70:3000/api/auth';
 
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 15000;
